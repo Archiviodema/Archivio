307652910af1bcbdaec8a1e841671fb4c76b3fbd
Diego Namkhai <d.namkhai@studenti.gobettivolta.gov.it>
2021-04-12 21:39:25 +0200

    change adduser and add

--- Archivio/web/add.php
+++ Archivio/web/add.php
@@ -1,13 +1,9 @@
+<?php include('../bin/starter.php');?>
+<?php include('../bin/reserved.php');?>
 <!DOCTYPE html>
 <html>
-
-<head>
-    <link rel='stylesheet' href='css/homepage.css' />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <script src="../bin/script-add_user.js"></script>
-    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
-    <title>Digital Arc</title>
-</head>
+<?php include('../html/header.php');?>
+<html>
 
 <body style="background-color: lightgray">
     <?php
@@ -45,33 +41,34 @@
         $CF = (new Calculator)->calcola($name, $surname, $gender, $date_of_birth_normalized, $codCatastale);
         $hashed_pwd = hash("sha512", $CF . $pwd);
         $stato = 0; // 0: Utente da aggiungere in DB, 1: Utente già Presente in DB -> modifica utente (password e ruolo)
-        $conn = new mysqli($servername, $username, $password, $dbname);
+        //$conn = new mysqli($servername, $username, $password, $dbname);
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
         $query = "SELECT user_CF from Utenti WHERE user_CF='" . $CF . "'";
-        $user = $conn->query($query) or die($conn->error);
-        $normalized_user = $user->fetch_assoc();
+        //$user = $conn->query($query) or die($conn->error);
+        //$normalized_user = $user->fetch_assoc();
+        $normalized_user = query($query);
         if ($normalized_user == null) {
             $stato = 0;
         }else $stato = 1;
         //echo $stato . " " . $normalized_user;
         if ($stato == 0){
             $query = 'INSERT INTO Utenti (user_CF, user_name, user_surname, user_pwd, user_role) VALUES ("' . $CF . '", "' . $name . '", "' . $surname . '", "' . $hashed_pwd . '", "' . $role . '")'; 
-            if ($conn->query($query) === TRUE) {
+            if ($conn->insert($query) === TRUE) {
                 $added = true;
               } else {
                 $added = false;
                 $error_msg = "Error: " . $query . "<br>" . $conn->error;
             }
             if(!$added){
-                echo '
+                ?>
                 <center>
                     <div id="main_div" class="jumbotron jumbotron-fluid" style="margin-left: 25%; margin-right: 25%; margin-top:1%; border-style: solid; border-width: 1px; border-color: #b8b8b8">
                         <div id="text_div" class="container">
                             <h1 class="display-4">Errore di connessione.</h1>
-                            <p class="lead">';
-                            echo $error_msg . '<br>Premi il pulsante per essere reindirizzato alla homepage.</p>
+                            <p class="lead">
+                            <?php echo $error_msg; ?>
                             <br><button class="btn btn-outline-primary mb-2" id="redirectBtn">Redirect</button>
                             <script language="javascript">var redirect = document.getElementById("redirectBtn");
                             redirect.addEventListener("mousedown", function() {
@@ -88,7 +85,8 @@
                             </script>
                         </div>
                     </div>
-                </center>';
+                </center>
+            <?php    
             }else{
                 // correctly done
                 header("Location: auto_redirect_homepage.php");
@@ -96,11 +94,12 @@
             }
         }else if ($stato == 1){
             $query = "SELECT * from Utenti WHERE user_CF='" . $CF . "'";
-            $user = $conn->query($query) or die($conn->error);
-            $normalized_user = $user->fetch_assoc();
+            //$user = $conn->query($query) or die($conn->error);
+            //$normalized_user = $user->fetch_assoc();
+            $normalized_user = query($query);
             $old_pwd = $normalized_user['user_pwd'];
             $del_query = "DELETE FROM Utenti WHERE user_CF='" . $CF . "'";
-            if ($conn->query($del_query) === TRUE) {
+            if ($conn->insert($del_query) === TRUE) {
                 $deleted = true;
             } else {
                 $deleted = false;
@@ -117,13 +116,15 @@
                     $error_msg = "Error: " . $query . "<br>" . $conn->error;
                 }
                 if(!$added){
-                    echo '
+                    ?>
                     <center>
                         <div id="main_div" class="jumbotron jumbotron-fluid" style="margin-left: 25%; margin-right: 25%; margin-top:1%">
                             <div id="text_div" class="container">
                                 <h1 class="display-4">Errore di connessione.</h1>
-                                <p class="lead"> (';
-                                echo $error_msg . ')<br>Premi il pulsante per essere reindirizzato alla homepage.</p>
+                                <p class="lead"> (
+                                <?php echo $error_msg ?> 
+                                )
+                                <br>Premi il pulsante per essere reindirizzato alla homepage.</p>
                                 <br><button class="btn btn-outline-primary mb-2" id="redirectBtn">Redirect</button>
                                 <script language="javascript">var redirect = document.getElementById("redirectBtn");
                                 redirect.addEventListener("mousedown", function() {
@@ -140,20 +141,22 @@
                                 </script>
                             </div>
                         </div>
-                    </center>';
+                    </center>
+                <?php    
                 }else{
                     // correctly done
                     header("Location: auto_redirect_homepage.php");
                     exit();
                 }
             }else{
-                echo '
+                ?>
                     <center>
                         <div id="main_div" class="jumbotron jumbotron-fluid" style="margin-left: 25%; margin-right: 25%; margin-top:1%">
                             <div id="text_div" class="container">
                                 <h1 class="display-4">Errore di connessione.</h1>
-                                <p class="lead">';
-                                echo $error_msg . '<br>Premi il pulsante per essere reindirizzato alla homepage.</p>
+                                <p class="lead">
+                                <?php echo $error_msg ?>
+                                <br>Premi il pulsante per essere reindirizzato alla homepage.</p>
                                 <br><button class="btn btn-outline-primary mb-2" id="redirectBtn">Redirect</button>
                                 <script language="javascript">var redirect = document.getElementById("redirectBtn");
                                 redirect.addEventListener("mousedown", function() {
@@ -170,7 +173,8 @@
                                 </script>
                             </div>
                         </div>
-                    </center>';
+                    </center>
+                    <?php
             }
             
         }
--- Archivio/web/add_user.php
+++ Archivio/web/add_user.php
@@ -1,52 +1,35 @@
+<?php include('../bin/starter.php');?>
+<?php include('../bin/reserved.php');?>
 <!DOCTYPE html>
 <html>
-
-<head>
-    <link rel='stylesheet' href='css/add_user.css' />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <script src="../bin/script-add_user.js"></script>
-    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
-    <title>Digital Arc</title>
-</head>
+<?php include('../html/header.php');?>
+<html>
 
 <body onload="bind_btn()" style="background-color: lightgray">
     <?php
-        include('../bin/config.php');
-        session_start();
-        $valid_session = true;
-        //echo count($_SESSION);
-        if(count($_SESSION) == 0){
-            $valid_session = false;
-        }
-        foreach($_SESSION as $key => $value){
-            if ($value == null) $valid_session = false;
-            // echo $key . ' ' . $value;
-        }
-        if($valid_session == true){
-            $session_user = $_SESSION['session_user'];
-            $conn = new mysqli($servername, $username, $password, $dbname);
+            //$conn = new mysqli($servername, $username, $password, $dbname);
             $query = "SELECT user_CF, user_name, user_surname, user_pwd, user_role from Utenti WHERE user_CF='" . $session_user->get_CF() . "' and user_pwd='" . $session_user->get_pwd() . "' and user_role='admin'";
-            $_is_admin = $conn->query($query) or die($conn->error);
-            $is_admin = $_is_admin->fetch_assoc();
+            $_is_admin = $conn->query($query);
+            //$is_admin = $_is_admin->fetch_assoc();
             if($is_admin != null){
                 // the current user is an admin
-                echo'
+                ?>
+
                 <center>
                     <div id="main_div" class="jumbotron jumbotron-fluid" style="margin-left: 25%; margin-right: 25%; margin-top:1%; border-style: solid; border-width: 1px; border-color: #b8b8b8">
                         <div id="text_div" class="container">
                             <h1 class="display-4">Inserimento Utente.</h1>
                             <p class="lead">Inserisci i dati dell\'utente.</p>
                             <hr class="my-4">
-                            <div id=\'input_div\'>
-                                <form  class=\'center\' style="display: inline" class="form-inline" action="add.php" method="GET">
+                            <div id='input_div'>
+                                <form  class='center' style="display: inline" class="form-inline" action="add.php" method="GET">
                                 <div class="form-group row">
                                     <label for="name" class="col-sm-2 col-form-label">Nome:</label>
-                                    <input class="form-control mb-2 mr-sm-2" name=\'name\' placeholder="Inserisci il Nome." id="name"
-                                        style=\'width: 25%\' autocomplete="off" >
+                                    <input class="form-control mb-2 mr-sm-2" name='name' placeholder="Inserisci il Nome." id="name"
+                                        style='width: 25%' autocomplete="off" >
                                     <label for="surname" class="col-sm-2 col-form-label">Cognome:</label>
-                                    <input class="form-control mb-2 mr-sm-2" name=\'surname\' placeholder="Inserisci il Cognome." id="surname"
-                                        style=\'width: 25%\' autocomplete="off" >
+                                    <input class="form-control mb-2 mr-sm-2" name='surname' placeholder="Inserisci il Cognome." id="surname"
+                                        style='width: 25%' autocomplete="off" >
                                 </div>
                                 <div class="form-check row">
                                     <div id="gender_div">
@@ -59,17 +42,17 @@
                                 <br>
                                 <div class="form-group row">
                                     <label class="col-sm-2 col-form-label" for="date_of_birth">Data di Nascita:</label>
-                                    <input class="form-control" type="date" id="date_of_birth" name="date_of_birth" style=\'width: 25%\' >
+                                    <input class="form-control" type="date" id="date_of_birth" name="date_of_birth" style='width: 25%' >
                                     <label for="place_of_birth" class="col-sm-2 col-form-label">Luogo di Nascita:</label>
-                                    <input class="form-control mb-2 mr-sm-2" name=\'place_of_birth\' placeholder="Inserisci il Luogo di Nascita." id="place_of_birth"
-                                        style=\'width: 25%\' autocomplete="off" >
+                                    <input class="form-control mb-2 mr-sm-2" name='place_of_birth' placeholder="Inserisci il Luogo di Nascita." id="place_of_birth"
+                                        style='width: 25%' autocomplete="off" >
                                     <label for="initial" class="col-sm-2 col-form-label">Sigla:</label>
-                                    <input class="form-control mb-2 mr-sm-2" name=\'initial\' placeholder="Inserisci la Sigla della Provincia" id="initial"
-                                        style=\'width: 25%\' autocomplete="off" >
+                                    <input class="form-control mb-2 mr-sm-2" name='initial' placeholder="Inserisci la Sigla della Provincia" id="initial"
+                                        style='width: 25%' autocomplete="off" >
                                 </div>
                                 <div class="form-group row">
                                     <label class="col-sm-2 col-form-label" for="password">Password Temporanea:</label>
-                                    <input class="form-control" type="text" id="password" name="password" style=\'width: 25%\' autocomplete="off" >
+                                    <input class="form-control" type="text" id="password" name="password" style='width: 25%' autocomplete="off" >
                                     <label class="col-sm-2 col-form-label" for="select_role">Ruolo:</label>
                                     <select class="form-control mb-2 mr-sm-2" id="select_role" name="role" style="width:25%" >
                                         <option id="select_admin">admin</option>
@@ -81,15 +64,12 @@
                             </div>
                         </div>
                     </div>
-                </center>';
+                </center>
+            <?php
             }else{
                 header("Location: redirect.php");
                 exit();
             }
-        }else{
-            header("Location: redirect.php");
-            exit();
-        }
         
         ?>
 </body>
